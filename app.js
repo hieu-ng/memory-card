46ac63b538143a1ef0fb98da22e31cdca235fb17
hieu-ng <hieutnguyen06@gmail.com>
2020-06-01 10:11:59 +1000

    Create a card and push to DOM

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ const cardsData = getCardsData();
 function createCards() {
 	cardsData.forEach((data, index) => createCard(data, index));
 }
+
+// Create a single card in DOM
+function createCard(data, index) {
+	const card = document.createElement('div');
+	card.classList.add('card');
+
+	if (index === 0) {
+		card.classList.add('active');
+	}
+
+	card.innerHTML =
+		`
+  <div class="inner-card">
+  <div class="inner-card-front">
+    <p>
+      ${data.question}
+    </p>
+  </div>
+  <div class="inner-card-back">
+    <p>
+      ${data.answer}
+    </p>
+  </div>
+</div>
+  `;
+
+	card.addEventListener('click', () => card.classList.toggle('show-answer'));
+
+	// Add to DOM cards
+	cardsEl.push(card);
+
+	cardsContainer.appendChild(card);
+
+	updateCurrentText();
+}
