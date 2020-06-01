fd1af404e941f1bf3822099fff27c728df215068
hieu-ng <hieutnguyen06@gmail.com>
2020-06-01 10:12:20 +1000

    Show number of cards

--- app.js
+++ app.js
@@ -58,3 +58,8 @@ function createCard(data, index) {
 
 	updateCurrentText();
 }
+
+// Show number of cards
+function updateCurrentText() {
+	currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
+}
